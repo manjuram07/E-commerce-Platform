a7c10c9633e9cfb185bacbba71b8f0a60f9c1db3
manjuram07 <khmanjunatha405@gmail.com>
2026-06-03 14:39:22 +0530

    code optmized

--- Product-Service/src/main/java/com/infy/ekart/product/ProductApplication.java
+++ Product-Service/src/main/java/com/infy/ekart/product/ProductApplication.java
@@ -3,7 +3,6 @@ package com.infy.ekart.product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 //@PropertySource(value = { "classpath:messages.properties" })
